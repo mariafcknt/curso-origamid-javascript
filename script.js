143862d02b47c1c5199e5e7affdaaf2b25dc095f
Maria <mariafcknt@outlook.com>
2026-05-28 16:46:37 -0300

    aula de dimensão e distancias e eventos

--- script.js
+++ script.js
@@ -1,73 +1,10 @@
-const listaAnimais = document.querySelector('.animais-lista')
-const height = listaAnimais.scrollHeight
-const animaisTop = listaAnimais.offsetTop
-console.log(height)
-console.log(animaisTop)
+const lista = document.querySelector('.animais-lista')
 
-const primeiroh2 = document.querySelector('h2')
-console.log(primeiroh2.offsetLeft)
+console.log(lista.children[--lista.children.length])
+console.log(lista.querySelector('li:last-child'))
 
-const h2rect = primeiroh2.getBoundingClientRect()
-console.log(h2rect.top)
+const animais = document.querySelector('.animais')
+const contato = document.querySelector('.contato')
+const tituloContato = contato.querySelector('.titulo')
 
-if(h2rect.top < 0) {
-    console.log('Passou do elemento')
-}
-
-console.log(
-    window.innerWidth,
-    window.outerWidth,
-    window.innerHeight,
-    window.outerHeight,
-    window.pageYOffset
-)
-
-const small = window.matchMedia('(max-width: 600px')
-
-if(small) {
-    console.log('Usuário mobile')
-} else {
-    console.log('Usuário desktop')
-}
-
-console.log(small)
-
-//EXERCÍCIOS
-
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
-
-const firstImg = document.querySelector('img')
-console.log(firstImg.offsetTop)
-
-// Retorne a soma da largura de todas as imagens
-
-const listImgs = document.querySelectorAll('img')
-
-let somaWidth = 0
-listImgs.forEach((img) => {
-    somaWidth += img.width
-})
-
-console.log(somaWidth)
-
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-
-const listLinks = document.querySelectorAll('a')
-
-console.log(listLinks)
-
-listLinks.forEach((link) => {
-    if (link.offsetHeight >= 44 && link.offsetWidth >= 44) {   
-        console.log('Possui o tamanho mínimo recomendado')
-    } else {
-        console.log(link.offsetHeight, link.offsetWidth)
-        console.log('Não possui o tamanho mínimo')
-    }
-})
-
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
-
+animais.appendChild(tituloContato) //adiciona tituloContato como último filho de animais
