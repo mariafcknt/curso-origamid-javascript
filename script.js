bc8f1bdcc7b1a8f2c644fe8c4e3ac2ca8ade3e98
Maria <mariafcknt@outlook.com>
2026-06-08 11:47:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -71,11 +71,11 @@ initScrollSuave()
 function initAnimacaoScroll() {
     const sections = document.querySelectorAll('.js-scroll')
     if (sections.length) {
-        const windowMetade = window.innerHeight * 0.6
+        const windowMetade = window.innerHeight * 0.6 //altura visível da tela do navegador
 
         function animaScroll() {
             sections.forEach((section) => {
-                const sectionTop = section.getBoundingClientRect().top 
+                const sectionTop = section.getBoundingClientRect().top //retorna informações sobre a posição do elemento na tela (.top é a distância do topo da janela até o topo da seção)
                 const isSectionVisible = (sectionTop - windowMetade) < 0
                 if (isSectionVisible) {
                     section.classList.add('ativo')
