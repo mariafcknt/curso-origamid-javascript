d04836c43cf08463af7c4dee44698d5b5bf6c625
Maria <mariafcknt@outlook.com>
2026-06-17 17:44:49 -0300

    math number class

--- number-math/script.js
+++ number-math/script.js
@@ -1,88 +1,99 @@
-// const htmlText = '<div>O melhor item</div><div>A melhor lista</div>'
-// console.log(htmlText)
+// // parseFloat() && parseInt() - retorna um número a partir de uma string
+// console.log(parseFloat('32424.45')) // 32424.45
+// console.log(parseFloat('32424,45')) // 32424
+// console.log(parseFloat('100 reais')) // 100
 
-// const htmlArray = htmlText.split('div') //usa o 'div' para separar a string em um array
-// console.log(htmlArray)
+// console.log(parseInt(23.9)) // 23
+// console.log(parseInt('100.25 reais')) // 100
 
-// const novoHtml = htmlArray.join('section') //junta a array de cima em uma string com os itens unidos por 'section)
-// console.log(novoHtml)
+// // toFixed() - arredonda o número com base no total de casas decimais no argumento
+// const preco = 2.99
+// console.log(preco.toFixed()) // 3
+// console.log(+preco.toFixed() + 2) // +preco - converte string de volta para número 
 
-// const valor = ' R$ 23,90 ' 
-// console.log(valor.trim()) //'R$ 23,90' - remove os espaços do começo e do final
-// console.log(valor.trimStart()) //'R$ 23,90 ' 
-// console.log(valor.trimEnd()) //' R$ 23,90'
+// const carro = 1000.455
+// console.log(carro.toFixed(2)) // 1000.46
 
-// Utilizando o foreach na array abaixo,
-// some os valores de Taxa e os valores de Recebimento
+// const preco2 = 1499.49
+// console.log(preco2.toFixed()) // 1499 - arredondou pra baixo
 
-const transacoes = [
-  {
-    descricao: 'Taxa do Pão',
-    valor: 'R$ 39',
-  },
-  {
-    descricao: 'Taxa do Mercado',
-    valor: 'R$ 129',
-  },
-  {
-    descricao: 'Recebimento de Cliente',
-    valor: 'R$ 99',
-  },
-  {
-    descricao: 'Taxa do Banco',
-    valor: 'R$ 129',
-  },
-  {
-    descricao: 'Recebimento de Cliente',
-    valor: 'R$ 49',
-  },
-];
+// console.log((99.99).toFixed()) //100
 
-let totalTaxa = 0
-let totalRecebimento = 0
-transacoes.forEach((item) => {
-    const valorLimpo = item.valor.slice(3)
-    const novoValor = Number(valorLimpo)
-    if (item.descricao.includes('Taxa')) {
-        totalTaxa += novoValor
-    } else if (item.descricao.includes('Recebimento')){
-        totalRecebimento += novoValor
+// // toLocaleString(lang, options) - formata um número de acordo com a língua e opções passadas
+// let valor = 48.49
+// valor = valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+// console.log(valor)
+
+// //Math - objeto com funções matemáticas
+// console.log(Math.PI) //retorna o valor de Pi
+// console.log(Math.abs(-5.5)) //5.5 - retorna o valor absoluto
+// console.log(Math.ceil(4.8334)) //5 - arredonda para um valor acima
+// console.log(Math.floor(4.8334)) //4 - arredonda para um valor abaixo
+// console.log(Math.round(4.8334)) //5 - arredonda para o inteiro mais próximo
+// console.log(Math.round(4.3)) //4
+
+// console.log(Math.max(4,5,34,54)) // 54 - retorna o valor maior
+// console.log(Math.min(4,5,34,54)) // 4 - retorna o valor menor
+
+// console.log(Math.random()) // gera um número aleatório entre 0 e 1
+// console.log(Math.random() * 100) // gera um número aleatório entre 0 e 100
+// console.log(Math.random() * (72 - 32 + 1) + 32) // gera um número aleatório entre 72 e 32 (max - min + 1) + min)
+
+// Retorne um número aleatório
+// entre 1050 e 2000
+console.log(Math.random() * (2000 - 1050 + 1) + 1050)
+
+// Retorne o maior número da lista abaixo
+const numeros = '4, 5, 20, 8, 9';
+let arrayNumeros = numeros.split(',')
+
+let maiorNumero = 0;
+arrayNumeros.forEach((numero) => {
+  numero = Number.parseInt(numero)
+  //console.log(numero)
+  if (numero > maiorNumero) {
+    maiorNumero = numero
+  }
+});
+
+console.log(maiorNumero)
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ['R$ 59,99', ' R$ 100,222',
+                     'R$ 230  ', 'r$  200'];
+
+// let somaPrecos = 0;                     
+// listaPrecos.forEach((preco) => {
+//   preco = preco.replace(/[ ]+/g, '')
+//   preco = preco.split('$')
+//   let valor = preco[1]
+//   if (valor) {
+//     valor = valor.replace(/[,]+/g, '.')
+//     valor = Number.parseFloat(valor)
+//     //ao multiplicar torna o valor um inteiro, e ao dividir o valor torna a ser decimal/adiciona uma casa decimal
+//     valor = Math.round(valor * 100) / 100 
+//     console.log(valor)
+//     somaPrecos += valor
+//   }
+// })  
+
+function somarPreco(lista) {
+  let somaPrecos = 0;
+  lista.forEach((preco) => {
+    preco = preco.replace(/[ ]+/g, '')
+    preco = preco.split('$')
+    let valor = preco[1]
+    if (valor) {
+      valor = valor.replace(/[,]+/g, '.')
+      valor = Number.parseFloat(valor)
+      valor = valor.toFixed(2)
+      valor = Number.parseFloat(valor)
+      somaPrecos += valor
     }
-})
+  })
+  return somaPrecos
+}
 
-console.log(totalTaxa)
-console.log(totalRecebimento)
-
-// Retorne uma array com a lista abaixo
-const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
-
-const arrayTransportes = transportes.split(';')
-console.log(arrayTransportes)
-
-// Substitua todos os span's por a's
-const html = `<ul>
-                <li><span>Sobre</span></li>
-                <li><span>Produtos</span></li>
-                <li><span>Contato</span></li>
-              </ul>`;
-//
-const novoHtml = html.split('span').join('a')
-console.log(novoHtml)
-
-// Retorne o último caracter da frase
-const frase = 'Melhor do ano!';
-console.log(frase.charAt(frase.length - 1))
-
-// Retorne o total de taxas
-const arrayTransacoes = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
-
-let somaQtdeTaxas = 0
-arrayTransacoes.forEach((item) => {
-    item = item.toLowerCase().trim() //trim() recebe o retorno de toLowerCase() como argumento (valor enviado para a função)
-    if (item.includes('taxa')) {
-        console.log(item)
-        somaQtdeTaxas++
-    }
-})
-
-console.log(somaQtdeTaxas)
+console.log(somarPreco(listaPrecos))
