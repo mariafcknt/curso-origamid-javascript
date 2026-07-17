8e63cfbdf94a802da017dbb50382a1236fc4e862
Maria <mariafcknt@outlook.com>
2026-07-17 16:36:05 -0300

    finish animaNumeros

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,29 +1,33 @@
 export default function initAnimaNumeros() {
+    function animaNumeros() {
+        const numeros = document.querySelectorAll('[data-numero]')
 
-}
+        numeros.forEach(numero => {
+            const total = +numero.innerText
+            const incremento = Math.floor(total / 100)
 
-const numeros = document.querySelectorAll('[data-numero]')
+            let start = 0
+            const timer = setInterval(() => {
+                start = start + incremento
+                numero.innerText = start
+                if (start > total) {
+                    numero.innerText = total
+                    clearInterval(timer)
+                }
+            }, 25 * Math.random())
+        })
+    }
 
-numeros.forEach(numero => {
-    const total = +numero.innerText
-    const incremento = Math.floor(total / 100)
-
-    let start = 0
-    const timer = setInterval(() => {
-        start = start + incremento
-        numero.innerText = start
-        if(start > total){
-            numero.innerText = total
-            clearInterval(timer)
+    function handleMutation(mutation) {
+        if (mutation[0].target.classList.contains('ativo')) {
+            observer.disconnect()
+            animaNumeros()
         }
-    }, 25 * Math.random())
-})
+    }
 
-function handleMutation() {
+    const observerTarget = document.querySelector('.numeros')
+    const observer = new MutationObserver(handleMutation)
 
+    observer.observe(observerTarget, { attributes: true })
 }
 
-const observerTarget = document.querySelector('.numeros')
-const observer = new MutationObserver(handleMutation)
-
-observer.observe(observerTarget, {attributes: true})
