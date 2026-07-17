8a7ed6e53fed42b71fa39a5a3ffa12c06c5cedab
Maria <mariafcknt@outlook.com>
2026-07-17 12:00:38 -0300

    doing numeros section

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ import initTabNav from "./modules/tab-nav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js"
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
 
 // import { teste1, teste2 } from "./modules/teste.js";
 // import * as teste from "./modules/teste.js";
@@ -20,4 +22,6 @@ initTabNav()
 initModal()
 initTooltip()
 initDropdownMenu()
+initMenuMobile()
+initAnimaNumeros()
 
