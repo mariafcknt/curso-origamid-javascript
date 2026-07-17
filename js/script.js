b0c63fd62843e475008735fe80699ddfaede7f97
Maria <mariafcknt@outlook.com>
2026-07-17 17:04:23 -0300

    finish funcionamento function

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js"
 import initMenuMobile from "./modules/menu-mobile.js";
 import initAnimaNumeros from "./modules/anima-numeros.js";
+import initFuncionamento from "./modules/funcionamento.js";
 
 // import { teste1, teste2 } from "./modules/teste.js";
 // import * as teste from "./modules/teste.js";
@@ -24,4 +25,4 @@ initTooltip()
 initDropdownMenu()
 initMenuMobile()
 initAnimaNumeros()
-
+initFuncionamento()
