781a744cd5d936d38a1dd58560a8d640a70aa39d
Maria <mariafcknt@outlook.com>
2026-08-13 16:04:52 -0300

    eslint, babel and webpack

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,30 +1,29 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import initAnimaNumeros from './anima-numeros.js'
 
 export default function initFetchAnimais() {
-    async function fetchAnimais(url) {
-        try {
-            const animaisResponse = await fetch(url)
-            const animaisJSON = await animaisResponse.json()
-            const numerosGrid = document.querySelector('.numeros-grid')
+  async function fetchAnimais(url) {
+    try {
+      const animaisResponse = await fetch(url)
+      const animaisJSON = await animaisResponse.json()
+      const numerosGrid = document.querySelector('.numeros-grid')
 
-            animaisJSON.forEach(animal => {
-                const divAnimal = createAnimal(animal)
-                numerosGrid.appendChild(divAnimal)
-            })
-            initAnimaNumeros()
-        } catch(erro) {
-            console.log(erro)
-        }
+      animaisJSON.forEach((animal) => {
+        const divAnimal = createAnimal(animal)
+        numerosGrid.appendChild(divAnimal)
+      })
+      initAnimaNumeros()
+    } catch (erro) {
+      console.log(erro)
     }
+  }
 
-    function createAnimal(animal) {
-        const div = document.createElement('div')
-        div.classList.add('numero-animal')
+  function createAnimal(animal) {
+    const div = document.createElement('div')
+    div.classList.add('numero-animal')
 
-        div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`
-        return div
-    }
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`
+    return div
+  }
 
-    fetchAnimais('./animais-api.json')
+  fetchAnimais('./animais-api.json')
 }
-
--- js/modules/teste.js
+++ js/modules/teste.js
@@ -1,9 +1,9 @@
-export function teste1 () {
-    console.log('Teste 1 ')
+export function teste1() {
+  console.log('Teste 1 ')
 }
 
-export function teste2 () {
-    console.log('Teste 2')
+export function teste2() {
+  console.log('Teste 2')
 }
 
 export const senha = 324232325
